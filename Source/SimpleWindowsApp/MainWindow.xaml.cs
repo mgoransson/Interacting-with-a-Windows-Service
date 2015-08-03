b9f05f67bf9a67649f1363cc4d68105fee45d296
mgoransson <marcus.goransson@yahoo.se>
2015-08-03 21:48:07 +0200

    Added ManagementEventWatcher

--- Source/SimpleWindowsApp/MainWindow.xaml.cs
+++ Source/SimpleWindowsApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,16 +24,40 @@ namespace SimpleWindowsApp
     {
         public const string ServiceName = "SimpleWindowsService";
         public ServiceController ServiceController { get; set; }
+        public ManagementEventWatcher EventWatcher { get; set; }
 
 
         public MainWindow()
         {
             InitializeComponent();
 
+            EventQuery eventQuery = new EventQuery();
+            eventQuery.QueryString = string.Format("SELECT * FROM __InstanceModificationEvent within 2 WHERE targetinstance isa 'Win32_Service' and targetinstance.name = '{0}'", ServiceName);
+            EventWatcher = new ManagementEventWatcher(eventQuery);
+            EventWatcher.Options.Timeout = new TimeSpan(1, 0, 0);
+            EventWatcher.EventArrived += new EventArrivedEventHandler(eventWatcher_EventArrived);
+            EventWatcher.Start();
+
             ServiceController = new ServiceController();
             ServiceController.ServiceName = ServiceName;
 
+            lblServiceStatus.Content = ServiceController.Status.ToString();
+
             changeBtnState();
+        }
+
+        void eventWatcher_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject targetInstance = ((ManagementBaseObject)e.NewEvent["targetinstance"]);
+            PropertyData state = targetInstance.Properties["state"];
+
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                lblServiceStatus.Content = state.Value.ToString();
+                btnStartService.IsEnabled = state.Value.ToString() == ServiceControllerStatus.Stopped.ToString();
+                btnStopService.IsEnabled = state.Value.ToString() == ServiceControllerStatus.Running.ToString();
+                btnRestartService.IsEnabled = state.Value.ToString() == ServiceControllerStatus.Running.ToString();
+            }));
         }
 
         private void btnStartService_Click(object sender, RoutedEventArgs e)
@@ -51,9 +76,8 @@ namespace SimpleWindowsApp
             {
                 System.Diagnostics.Debug.WriteLine(ex.Message);
                 lblError.Content = ex.Message;
+                changeBtnState();
             }
-
-            changeBtnState();
         }
 
         private void btnStopService_Click(object sender, RoutedEventArgs e)
@@ -72,9 +96,8 @@ namespace SimpleWindowsApp
             {
                 System.Diagnostics.Debug.WriteLine(ex.Message);
                 lblError.Content = ex.Message;
+                changeBtnState();
             }
-
-            changeBtnState();
         }
 
         private void btnRestartService_Click(object sender, RoutedEventArgs e)
@@ -94,8 +117,8 @@ namespace SimpleWindowsApp
             {
                 System.Diagnostics.Debug.WriteLine(ex.Message);
                 lblError.Content = ex.Message;
+                changeBtnState();
             }
-            changeBtnState();
         }
 
         private void changeBtnState()
@@ -103,6 +126,13 @@ namespace SimpleWindowsApp
             btnStartService.IsEnabled = ServiceController.Status == ServiceControllerStatus.Stopped;
             btnStopService.IsEnabled = ServiceController.Status == ServiceControllerStatus.Running;
             btnRestartService.IsEnabled = ServiceController.Status == ServiceControllerStatus.Running;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            EventWatcher.Stop();
+            EventWatcher.Dispose();
+            EventWatcher = null;
         }
     }
 }
